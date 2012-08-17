b17df041a893b716c8b23fd5f5fd528528a14a85
cesine <cesine@yahoo.com>
2012-08-17 09:59:39 -0400

    adding Android app structure and chrome app structure and test infrastructure fixes #3 and fixes #1 and fixes #10

---
+++ src/com/github/childphonologylabmcgill/android/sails/SAILSActivity.java
@@ -0,0 +1,13 @@
+package com.github.childphonologylabmcgill.android.sails;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SAILSActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
