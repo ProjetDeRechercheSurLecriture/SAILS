f99ea4c205348130e89fdeac267b4216ab23f81c
cesine <cesine@yahoo.com>
2012-08-17 12:19:49 -0400

    creating TCPP android files

--- src/com/github/childphonologylabmcgill/android/tcpp/TCPPActivity.java
+++ src/com/github/childphonologylabmcgill/android/tcpp/TCPPActivity.java
@@ -1,9 +1,11 @@
-package com.github.childphonologylabmcgill.android.sails;
+package com.github.childphonologylabmcgill.android.tcpp;
+
+import com.github.childphonologylabmcgill.android.tcpp.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SAILSActivity extends Activity {
+public class TCPPActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
