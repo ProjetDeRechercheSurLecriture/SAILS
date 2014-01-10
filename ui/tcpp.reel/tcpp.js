ff0aa7ee3821459c9672a2399d85fa99d20ccdba
cesine <cesine@yahoo.com>
2014-01-10 11:46:59 -0500

    created a tcpp component

---
+++ ui/tcpp.reel/tcpp.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tcpp.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tcpp
+ * @extends Component
+ */
+exports.Tcpp = Component.specialize(/** @lends Tcpp# */ {
+    constructor: {
+        value: function Tcpp() {
+            this.super();
+        }
+    }
+});
