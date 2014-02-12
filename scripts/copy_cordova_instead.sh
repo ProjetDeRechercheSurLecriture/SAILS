eaafa4aeccf6fac05381a0dc4f0f5bed51f1151a
cesine <cesine@yahoo.com>
2014-02-12 11:35:54 -0500

    was able to hack in cordova audio sample for ProjetDeRechercheSurLecriture/DyslexDisorthGame#140

--- scripts/copy_cordova_instead.sh
+++ scripts/copy_cordova_instead.sh
@@ -4,6 +4,7 @@ mop --version || {sudo npm install -g mop }
 mop
 
 cp background_cordova.js builds/pre-sails/background.js
+cp node_modules/oprime-montage/lib/cordova-bridge.js builds/pre-sails/cordova-bridge.js 
 
 echo "Removing extra stuff from builds to reduce apk size"
 echo 'TODO figure out what this does:   "bundles": ["ui/main.reel"], '
@@ -11,3 +12,9 @@ echo 'TODO figure out what this does:   "bundles": ["ui/main.reel"], '
 cd  builds/pre-sails/packages
 find . -name  "*[^d].js" -delete
 find . -name  "*.md" -delete
+
+sed 's/<meta data-insertcordovahere="" \/>/<script type="text\/javascript" charset="utf-8" src="cordova.js"><\/script>\n\t<script type="text\/javascript" charset="utf-8" src="cordova-bridge.js"><\/script>/' ../index.html  > output
+mv output ../index.html
+
+# sed 's/cordova-bridge.js/cordova-bridge.load.js/' ../index.html  > output
+# mv output ../index.html
