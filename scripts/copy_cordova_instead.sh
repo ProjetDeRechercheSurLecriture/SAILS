81cf21235dd2105502386a10f193d161439040bf
cesine <cesine@yahoo.com>
2014-02-13 16:29:38 -0500

    commenting out the insert of cordova for ios developemnt

--- scripts/copy_cordova_instead.sh
+++ scripts/copy_cordova_instead.sh
@@ -15,8 +15,9 @@ echo 'TODO figure out what this does:   "bundles": ["ui/main.reel"], '
 cd  builds/pre-sails/packages && find . -name  "*[^d].js" -delete && find . -name  "*.md" -delete
 
 # ipad loads with this commented out
-sed 's/<meta data-insertcordovahere="" \/>/<script type="text\/javascript" charset="utf-8" src="cordova.js"><\/script><script type="text\/javascript" charset="utf-8" src="cordova-bridge.js"><\/script>/' ../index.html  > output
-mv output ../index.html
+# sed 's/<meta data-insertcordovahere="" \/>/<script type="text\/javascript" charset="utf-8" src="cordova.js"><\/script><script type="text\/javascript" charset="utf-8" src="cordova-bridge.js"><\/script>/' ../index.html  > output
+# mv output ../index.html
 
+# The montage version makes it not global. todo, do something more montage like to get it to work...
 # sed 's/cordova-bridge.js/cordova-bridge.load.js/' ../index.html  > output
 # mv output ../index.html
