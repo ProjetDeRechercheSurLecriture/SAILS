d5a838d5cf6d1442814b9af8f85183564d606130
cesine <cesine@yahoo.com>
2014-02-13 15:45:43 -0500

    returning an error if mop fails

--- scripts/copy_cordova_instead.sh
+++ scripts/copy_cordova_instead.sh
@@ -1,7 +1,10 @@
 mop --version || {sudo npm install -g mop }
 
 # rm -rf builds
-mop
+mop || {
+	echo "mop failed"
+	exit 1;
+}
 
 cp background_cordova.js builds/pre-sails/background.js
 cp node_modules/oprime-montage/lib/cordova-bridge.js builds/pre-sails/cordova-bridge.js 
@@ -9,13 +12,11 @@ cp node_modules/oprime-montage/lib/cordova-bridge.js builds/pre-sails/cordova-br
 echo "Removing extra stuff from builds to reduce apk size"
 echo 'TODO figure out what this does:   "bundles": ["ui/main.reel"], '
 
-cd  builds/pre-sails/packages
-find . -name  "*[^d].js" -delete
-find . -name  "*.md" -delete
+cd  builds/pre-sails/packages && find . -name  "*[^d].js" -delete && find . -name  "*.md" -delete
 
 # ipad loads with this commented out
-# sed 's/<meta data-insertcordovahere="" \/>/<script type="text\/javascript" charset="utf-8" src="cordova.js"><\/script><script type="text\/javascript" charset="utf-8" src="cordova-bridge.js"><\/script>/' ../index.html  > output
-# mv output ../index.html
+sed 's/<meta data-insertcordovahere="" \/>/<script type="text\/javascript" charset="utf-8" src="cordova.js"><\/script><script type="text\/javascript" charset="utf-8" src="cordova-bridge.js"><\/script>/' ../index.html  > output
+mv output ../index.html
 
 # sed 's/cordova-bridge.js/cordova-bridge.load.js/' ../index.html  > output
 # mv output ../index.html
